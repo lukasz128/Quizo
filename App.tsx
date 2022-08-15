e36a7fddb6639323d4f83a8bd7de9c93142f1e94
Łukasz Fereniec <lukaszfereniec@interia.pl>
2022-08-15 19:34:07 +0200

    react navigation has been installed and implemented basic route

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {View} from 'react-native';
@@ -8,9 +9,11 @@ import Main from './src/Main';
 export default function App() {
   useDeviceContext(tw);
   return (
-    <View>
-      <Main />
-      <StatusBar style="light" />
-    </View>
+    <NavigationContainer>
+      <View style={tw`flex h-screen`}>
+        <Main />
+        <StatusBar style="light" />
+      </View>
+    </NavigationContainer>
   );
 }
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,15 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import HomePage from './views/HomePage/HomePage';
+import LoginPage from './views/LoginPage/LoginPage';
 
 export default function Main() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <>
-      {/* <LoginPage /> */}
-      <HomePage />
-    </>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="LoginPage" component={LoginPage} />
+      <Stack.Screen name="HomePage" component={HomePage} />
+    </Stack.Navigator>
   );
 }
